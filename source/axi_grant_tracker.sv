f69cb8becae472f57e39e931f962c62dd1c8ac72
chu282 <chu282@purdue.edu>
2026-05-17 23:27:39 -0700

    grant tracker

--- source/axi_grant_tracker.sv
+++ source/axi_grant_tracker.sv
@@ -1,12 +1,26 @@
 `timescale 1ns / 10ps
 
 module axi_grant_tracker #(
-    // parameters
+    parameter FIFO_DEPTH = 8
 ) (
-    input logic clk, n_rst
+    input  logic clk, n_rst, new_trans,
+    input  logic [NUM_SLAVES-1:0] trans_finished,
+    output logic full,
+    output logic [NUM_MASTERS-1:0] grant
 );
 
+    /*
+    One fifo per non-address channel, controlling grant of each slave
 
+    Push one on the top every time a new transactions starts
+
+    Pop one off every time a transaction finishes
+    */
+    axi_fifo #(.DEPTH(FIFO_DEPTH), .WIDTH(NUM_MASTERS)) fifo (
+        .push(new_trans),
+        .pop(trans_finished),
+        .top(grant),
+        .full(full), .*
+    );
 
 endmodule
-
